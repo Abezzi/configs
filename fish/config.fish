33e567ef1160db484b84ddb3d4a1236ebfc128ca
Alex Herrera <Abezzi@outlook.cl>
2025-11-02 10:15:10 -0300

    feat(fish): Configure Flyctl and Android SDK paths in config.fish
    Add environment variables and paths for Flyctl and Android SDK, disable greeting

--- fish/config.fish
+++ fish/config.fish
@@ -1,4 +1,16 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
     set -U fish_user_paths /home/wizwiz/go/bin
+    set -x FLYCTL_INSTALL "/home/wizwiz/.fly"
+    set -x PATH "$FLYCTL_INSTALL/bin" $PATH
+    # Android
+    set -gx ANDROID_HOME $HOME/Android/Sdk
+    fish_add_path $ANDROID_HOME/emulator
+    fish_add_path $ANDROID_HOME/platform-tools
+    fish_add_path $ANDROID_HOME/cmdline-tools/latest/bin
+    set -gx JAVA_HOME /usr/lib/jvm/java-24-openjdk
+    fish_add_path $JAVA_HOME/bin
 end
+
+alias v="nvim"
+set fish_greeting ""
